662400ce6bd61a2f99e786c7c95b4955bbdbe817
ssdplc1-crypto <ssdplc1@gmail.com>
2026-08-12 23:09:55 +0100

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,15 +1,16 @@
-const CACHE = "taskora-v2";
+const CACHE = "taskora-v3";
+
+const STATIC_ASSETS = [
+  "/static/style.css",
+  "/static/manifest.json",
+  "/static/icons/icon-192.png",
+  "/static/icons/icon-512.png"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        "/static/style.css",
-        "/static/manifest.json"
-      ])
-    )
+    caches.open(CACHE).then(cache => cache.addAll(STATIC_ASSETS))
   );
-
   self.skipWaiting();
 });
 
@@ -23,21 +24,32 @@ self.addEventListener("activate", event => {
       )
     )
   );
-
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
 
+  if (
+    event.request.mode === "navigate" ||
+    event.request.headers.get("accept")?.includes("text/html")
+  ) {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
   event.respondWith(
     fetch(event.request)
       .then(response => {
-        const copy = response.clone();
+        if (response.ok) {
+          const copy = response.clone();
 
-        caches.open(CACHE).then(cache => {
-          cache.put(event.request, copy);
-        });
+          caches.open(CACHE).then(cache => {
+            cache.put(event.request, copy);
+          });
+        }
 
         return response;
       })

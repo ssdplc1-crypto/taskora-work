848d49f4a73c9114a502594186a3b6b9c2b0d1a9
ssdplc1-crypto <ssdplc1@gmail.com>
2026-08-14 18:12:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ class DBConnection:
             return cur
         return self.conn.execute(sql, params)
 
-            def executecript(self, sql):
+                def executecript(self, sql):
         if self.is_postgres:
             cur = self.conn.cursor()
 
